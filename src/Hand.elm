c0fc2119ae1d3908f8f0d4426b1960c359308311
Qluxzz <andre.eric.andersson@gmail.com>
2022-12-11 22:05:51 +0100

    Add compare hands function

--- src/Hand.elm
+++ src/Hand.elm
@@ -4,21 +4,43 @@ import Card as Card
 
 
 type alias Hand =
-    { cards : List Card.Card
-    }
+    List Card.Card
 
 
 value : Hand -> ( Int, Int )
-value { cards } =
+value cards =
     List.foldl
         (\card ->
             \( h1, h2 ) ->
                 case Card.value card of
                     Card.Single v ->
-                        ( h1 + v, h2 )
+                        ( h1 + v, h2 + v )
 
                     Card.Double v1 v2 ->
                         ( h1 + v1, h2 + v2 )
         )
         ( 0, 0 )
         cards
+
+
+largestValidHandValue : Hand -> Int
+largestValidHandValue hand =
+    case value hand of
+        ( v1, v2 ) ->
+            if v2 > 21 then
+                v1
+
+            else
+                v2
+
+
+comp : Hand -> Hand -> Order
+comp a b =
+    let
+        aValue =
+            largestValidHandValue a
+
+        bValue =
+            largestValidHandValue b
+    in
+    Basics.compare aValue bValue
