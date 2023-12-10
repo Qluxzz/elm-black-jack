fa10c28a10d813bb103d2d9918a26e2266377f6f
Qluxzz <andre.eric.andersson@gmail.com>
2023-12-10 22:03:42 +0100

    Add tests for comparison

---
+++ tests/HandTests.elm
@@ -0,0 +1,37 @@
+module HandTests exposing (..)
+
+import Card
+import Expect
+import Hand
+import Test exposing (..)
+
+
+createCard : Card.Suit -> Card.Value -> Card.Card
+createCard s v =
+    Card.Card s v
+
+
+card : Card.Value -> Card.Card
+card =
+    createCard Card.Clubs
+
+
+cases : List ( ( Hand.Hand, Hand.Hand ), Order )
+cases =
+    [ ( ( [ card Card.Ace, card Card.King ], [ card Card.Ace, card Card.King ] ), EQ )
+    , ( ( [ card Card.Two, card Card.Three ], [ card Card.Two, card Card.Five ] ), LT )
+    , ( ( [ card Card.Two, card Card.Six ], [ card Card.Two, card Card.Five ] ), GT )
+    , ( ( [ card Card.Ace ], [ card Card.Ten ] ), GT )
+    ]
+
+
+suite : Test
+suite =
+    describe "Hand tests"
+        (List.map
+            (\( ( h1, h2 ), expected ) ->
+                test (Hand.toString h1 ++ " " ++ Hand.toString h2) <|
+                    \_ -> Hand.comp h1 h2 |> Expect.equal expected
+            )
+            cases
+        )
