1ba3639f89ba87ffc70d2a471a7ff66880ff79c5
Qluxzz <andre.eric.andersson@gmail.com>
2024-10-25 22:24:47 +0200

    Rename variables

--- src/Cards.elm
+++ src/Cards.elm
@@ -3,18 +3,18 @@ module Cards exposing (canSplit, comp, largestValue, toString)
 import Card
 
 
-{-| Get the value of a hand. A tuple is returned since Aces are worth 1/11. The first item of the tuple is counting the Ace as 1, the other as 11
+{-| Get the value of a list of cards. A tuple is returned since Aces are worth 1/11. The first item of the tuple is counting the Ace as 1, the other as 11
 -}
 value : List Card.Card -> ( Int, Int )
 value cards =
     List.foldl
-        (\card ( h1, h2 ) ->
+        (\card ( low, high ) ->
             case Card.value card of
                 Card.Single v ->
-                    ( h1 + v, h2 + v )
+                    ( low + v, high + v )
 
                 Card.Double v1 v2 ->
-                    ( h1 + v1, h2 + v2 )
+                    ( low + v1, high + v2 )
         )
         ( 0, 0 )
         cards
@@ -25,14 +25,14 @@ value cards =
 largestValue : List Card.Card -> Int
 largestValue cards =
     let
-        ( v1, v2 ) =
+        ( low, high ) =
             value cards
     in
-    if v2 > 21 then
-        v1
+    if high > 21 then
+        low
 
     else
-        v2
+        high
 
 
 comp : List Card.Card -> List Card.Card -> Order
